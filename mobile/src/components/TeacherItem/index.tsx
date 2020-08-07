7f2d60ba3ce5f5382d636714685bcbeee2859621
André Euzébio <andre.r.euzebio@gmail.com>
2020-08-07 16:07:23 -0300

    Listing teacher filtered through api call of backend application

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -8,28 +8,40 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 import styles from './styles';
 import { RectButton } from 'react-native-gesture-handler';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
                 <Image
                     style={styles.avatar}
-                    source={{ uri: 'http://github.com/andre-euz.png'}}                
+                    source={{ uri: teacher.avatar}}                
                 />
                 <View style={styles.profileInfo}>
-                    <Text style={styles.name}>André Euzébio</Text>
-                    <Text style={styles.subject}>Matemática</Text>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
                 </View>
             </View>
             <Text style={styles.bio}>
-                I'm a programmer about somewhat 7 years and I started working with programming 
-                {'\n'}{'\n'}
-                I’m currently working on Senior Sistemas with internal language                 
+                {teacher.bio}
             </Text>
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/hora {'   '}
-                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                    <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
                 </Text>
                 <View style={styles.buttonContainer}>
                         <RectButton style={[styles.favoriteButton, styles.favorited]}>
