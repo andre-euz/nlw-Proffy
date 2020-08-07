dabf8f662a2cdef0d38278797b470bacab6a7c39
André Euzébio <andre.r.euzebio@gmail.com>
2020-08-07 10:46:39 -0300

    Ending layout of teacher item page

--- mobile/src/components/TeacherItem/styles.ts
+++ mobile/src/components/TeacherItem/styles.ts
@@ -41,26 +41,50 @@ const styles = StyleSheet.create({
         lineHeight: 24,
         color: '#6a6180'
     },
-    footer: {
-
+    footer: {        
+        backgroundColor: '#fafafc',
+        padding: 24,
+        alignItems: 'center',
+        marginTop: 24
     },
     price: {
-
+        fontFamily: 'Poppins_400Regular',
+        color: '#6a6180',
+        fontSize: 14,
     },
     priceValue: {
-
+        fontFamily: 'Archivo_700Bold',
+        color: '#8257e5',
+        fontSize: 16
     },
     buttonContainer: {
-
+        flexDirection: 'row',
+        marginTop: 16
     },
     favoriteButton: {
-
+        backgroundColor: '#8257e5',
+        width: 56,
+        height: 56,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 8
     },
     contactButton: {
-
+        backgroundColor: '#04d361',
+        flex: 1,
+        height: 56,
+        borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 8
     },
     contactButtonText: {
-
+        color: '#FFF',
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 16,
+        marginLeft: 16
     },
 });
 
