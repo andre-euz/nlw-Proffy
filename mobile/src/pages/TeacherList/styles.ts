40410c5224fb66c8633f227005619e73f6e4e777
André Euzébio <andre.r.euzebio@gmail.com>
2020-08-07 14:44:21 -0300

    Ending teacher filters in the header page, added a button to toggle the filter fields and save space from screen

--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
         marginTop: -40,        
     },
     searchForm: {
-        marginBottom: 8
+        marginBottom: 24
     },
     label: {
         color: '#d4c2ff',
@@ -25,11 +25,26 @@ const styles = StyleSheet.create({
         marginBottom: 16
     },
     inputGroup: {
-
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     inputBlock: {
+        width: '48%'
 
     },
+    submitButton: {
+        backgroundColor: '#04d361',
+        height: 56,
+        borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'        
+    },
+    submitButtonText: {
+        color: '#FFF',
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 16,
+    }
 
 });
 
